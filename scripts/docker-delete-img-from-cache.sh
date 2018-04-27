0ffdd03d88b11f2695fcd6a4a91dc825a182f33d
Peter Bukva <peter.bukva@fetch.ai>
2018-04-27 10:30:02 +0100

    WIP: Fixing error handling in delete images script

--- scripts/docker-delete-img-from-cache.sh
+++ scripts/docker-delete-img-from-cache.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+set -e
+set -x
 
 SCRIPTS_DIR=${0%/*}
 . "$SCRIPTS_DIR"/docker-env-common.sh
 
 delete-image() {
     local IMG_TAG=$1
-    docker image inspect "$IMG_TAG" >/dev/null 2>&1 && docker rmi "$IMG_TAG"
+    docker image inspect "$IMG_TAG" >/dev/null 2>&1 && docker rmi "$IMG_TAG" || :
 }
 
 delete-dangling-layers() {
@@ -15,8 +17,11 @@ delete-dangling-layers() {
         docker rmi $LAYERS
     fi
 }
-
+echo start
 delete-image "$DOCKER_IMAGE_TAG"
+echo x1
 delete-image "$REGISTRY_DOCKER_IMAGE_TAG"
+echo x2
 delete-dangling-layers
+echo stop
 
