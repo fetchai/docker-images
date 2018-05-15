44d96cb4eb211f5e9be781401d16f695be92fc60
Peter Bukva <peter.bukva@fetch.ai>
2018-05-15 08:37:01 +0100

    Fixing outstanding issues related to sh posix standard

--- scripts/docker-delete-img-from-cache.sh
+++ scripts/docker-delete-img-from-cache.sh
@@ -4,10 +4,10 @@ SCRIPTS_DIR=${0%/*}
 . "$SCRIPTS_DIR"/docker-env-common.sh
 
 delete_dangling_layers() {
-    local LAYERS=$(docker images -f dangling=true -q)
+    local LAYERS="$(docker images -f dangling=true -q)"
     if [ -n "$LAYERS" ]
     then 
-        docker rmi $LAYERS
+        docker rmi -f $LAYERS
     fi
 }
 delete_image "$DOCKER_IMAGE_TAG"
