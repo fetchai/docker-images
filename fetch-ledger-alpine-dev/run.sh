8233dbac37f8efcd5185f0060abff2574b58438f
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2018-08-03 10:36:25 +0100

    Remove unused file

--- fetch-ledger-alpine-dev/run.sh
+++
@@ -1,9 +0,0 @@
-# #!/usr/bin/env bash
-# set -e
-
-# docker build -t fetch-ledger-alpine-dev .
-
-# docker run -v /Users/ed/Code/Cpp/fetch-ledger:/project:ro,cached \
-#            -v /Users/ed/Code/Cpp/fetch-ledger/build-alpine:/build \
-#            fetch-ledger-alpine-dev:latest \
-#            'pwd && cmake /project'
