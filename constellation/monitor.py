a616fc4f9f06d27582680181e12d3e3ba0022e8f
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2018-08-08 14:31:06 +0100

    Update monitor script to supply dependencies

--- constellation/monitor.py
+++ constellation/monitor.py
@@ -2,6 +2,7 @@
 import subprocess
 import time
 import sys
+import requests
 
 APPLICATION = '/app/constellation'
 
@@ -11,6 +12,10 @@ def output(text):
     sys.stderr.flush()
 
 
+# find out external ip address
+public_ip = requests.get('https://api.ipify.org').text
+print('Public IP: ', public_ip)
+
 n = 0
 while True:
     output('Starting application...')
@@ -18,7 +23,7 @@ while True:
     with open('/app/node-{}.log'.format(n), 'w') as node_log:
 
         # run the application
-        exit_code = subprocess.call([APPLICATION, '-bootstrap'], stdout=node_log, stderr=subprocess.STDOUT)
+        exit_code = subprocess.call([APPLICATION, '-bootstrap', public_ip], stdout=node_log, stderr=subprocess.STDOUT)
 
         if exit_code == 0:
             output('Stopping application...')
