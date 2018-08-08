c60018d8cab45defdcd132aa69963ce16bc36470
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2018-08-08 13:12:13 +0100

    Update monitor script to capture log output to file

--- constellation/monitor.py
+++ constellation/monitor.py
@@ -11,16 +11,22 @@ def output(text):
 	sys.stderr.flush()
 
 
+n = 0
 while True:
 	output('Starting application...')
-	exit_code = subprocess.call([APPLICATION])
 
-	if exit_code == 0:
-		output('Stopping application...')
-		break
+	with open('/app/node-{}.log'.format(n), 'w') as node_log:
+
+		# run the application
+		exit_code = subprocess.call([APPLICATION], stdout=node_log, stderr=subprocess.STDOUT)
+
+		if exit_code == 0:
+			output('Stopping application...')
+			break
 
 	output('Sleeping...')
 
 	time.sleep(30)
+	n += 1
 
 	output('Restarting application...')
