3f705337960bbf4af57fc1f77cc11b6e8a4685fa
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2018-10-02 19:53:20 +0100

    Minor testing update to the monitor script

--- constellation/monitor.py
+++ constellation/monitor.py
@@ -2,7 +2,10 @@
 import subprocess
 import time
 import sys
+import os
+import json
 import requests
+import base64
 
 APPLICATION = '/app/constellation'
 
@@ -12,26 +15,43 @@ def output(text):
     sys.stderr.flush()
 
 
+# get the initial configuration from the environment variable
+config = os.environ.get('CONSTELLATION_CONFIG')
+
+if config is None:
+    output('Configuration not specified')
+else:
+
+    # decode the configuration
+    config = base64.b64decode(config)
+    output('Config: ' + config)
+
+    # json parse the config
+    config = json.loads(config)
+
+# run the main application
+subprocess.check_call([APPLICATION, '-mine'])
+
 # find out external ip address
-public_ip = requests.get('https://api.ipify.org').text
-print('Public IP: ', public_ip)
+# public_ip = requests.get('https://api.ipify.org').text
+# output('Public IP: ', public_ip)
 
-n = 0
-while True:
-    output('Starting application...')
+# n = 0
+# while True:
+#     output('Starting application...')
 
-    with open('/app/node-{}.log'.format(n), 'w') as node_log:
+#     with open('/app/node-{}.log'.format(n), 'w') as node_log:
 
-        # run the application
-        exit_code = subprocess.call([APPLICATION, '-mine'], stdout=node_log, stderr=subprocess.STDOUT)
+#         # run the application
+#         exit_code = subprocess.call([APPLICATION, '-mine'], stdout=node_log, stderr=subprocess.STDOUT)
 
-        if exit_code == 0:
-            output('Stopping application...')
-            break
+#         if exit_code == 0:
+#             output('Stopping application...')
+#             break
 
-    output('Sleeping...')
+#     output('Sleeping...')
 
-    time.sleep(30)
-    n += 1
+#     time.sleep(30)
+#     n += 1
 
-    output('Restarting application...')
+#     output('Restarting application...')
