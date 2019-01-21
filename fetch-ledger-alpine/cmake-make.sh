8ea7d32407a79c6bcd853b41fd58745bf59cea32
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2019-01-21 12:30:36 +0000

    Update default build to be in RelWithDebInfo and to disable colour log

--- fetch-ledger-alpine/cmake-make.sh
+++ fetch-ledger-alpine/cmake-make.sh
@@ -4,6 +4,6 @@ CMAKE_BUILD_DIR=build-alpine
 mkdir -p "$CMAKE_BUILD_DIR"
 cd "$CMAKE_BUILD_DIR"
 
-cmake ..
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DFETCH_DISABLE_COLOUR_LOG=TRUE ..
 make -j "$@"
 
